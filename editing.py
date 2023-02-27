871482fd156116ab153bfb65b7d7cfb79c0567a5
nadezhda <nadezhda.bessonova@gmail.com>
2023-02-27 19:29:05 +0500

    редактирование ошибок

--- editing.py
+++ editing.py
@@ -13,6 +13,16 @@ def editing():
         else:
             edit_key = data_editing()
             flag = False
+        try:
+            what_edit =int(input('Что хотите изменить? (1 - название, 2 - содержание)'))
+            if what_edit != 1 or what_edit != 2:
+                print('Ошибка ввода')
+                return   
+        except ValueError:
+            print('Ошибка ввода')
+            return 
+            
+
         with open('Note.csv', 'r', encoding='utf-8') as note:
             old_data = note.readlines()
         with open('Note.csv', 'w', encoding='utf-8') as note:
@@ -27,7 +37,7 @@ def editing():
                     line_array=[]
                     line_array=line.split(';') 
                     super_input = inline.input
-                    what_edit =int(input('Что хотите изменить? (1 - название, 2 - содержание)'))
+                    
                     if  what_edit == 1:
                         note_name = super_input("Новое название: ", inp=line_array[1])
                         note.write('{};{};{};{}\n'.format(edit_key,note_name,line_array[2],date_note))
@@ -37,43 +47,6 @@ def editing():
                         note_body = super_input("Новое название: ", inp=line_array[2])
                         note.write('{};{};{};{}\n'.format(edit_key,line_array[1],note_body,date_note))
                         print('\nСодержание заметки №{} успешно изменено'.format(edit_key))
-                    else:
-                        print('Ошибка ввода')
-                        return
-
+                    
         if flag == False:
             print('\nЗаметки под №{} несуществует'.format(edit_key))  
-    # search()
-    # editing_key = data_editing()
-    # # flag = False
-    # with open('Note.csv', 'r', encoding='utf-8') as note:
-    #     old_data = note.readlines()
-    # with open('Note.csv', 'w', encoding='utf-8') as note:
-    #     for i, line in enumerate(old_data):
-    #         if line.split(';')[0] != editing_key:
-    #             note.write(line)
-    #         else:
-    #             print('{}'.format(line))
-
-    #             print('Что хотите изменить? (1 - название, 2 - содержание)')
-                # to_add = []
-                # print("\nРедактирование заметки №{}n".format(editing_key))
-                # to_add.append(input('Введите новое название: '))
-                # to_add.append(input('Введите новое содержание: '))
-                # dateNote = datetime.datetime.now().strftime("%d-%m-%Y,%H:%M:%S")
-                # # save = input('Сохранить данную заметку? (1 - да, 2 - нет): ')
-                # note.write('{};{};{};{}\n'.format(editing_key, to_add[0], to_add[1], dateNote))
-    #             flag = True
-    # if flag == True:
-    #     print('\nЗаметка №{} успешно удалена'.format(edit_key))
-    # else:
-    #     print('\nЗаметки под №{} несуществует'.format(edit_key))       
-# from exceptions import data_editing
-
-# with open ('Note.csv', 'r') as note:
-#   old_data = note.readlines()
-#   for i in old_data:
-
-# new_data = old_data.replace('что_меняем', 'на_что_меняем')
-# with open ('Note.txt', 'w') as note:
-#   note.write(new_data)
